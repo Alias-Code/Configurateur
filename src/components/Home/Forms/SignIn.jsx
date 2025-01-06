905658e239f5b55e9176062d381f8f5946e1bd9e
Alias Code <alias.bcll@gmail.com>
2025-01-06 12:05:28 +0100

    Update

--- src/components/Home/Forms/SignIn.jsx
+++ src/components/Home/Forms/SignIn.jsx
@@ -257,7 +257,7 @@ export default function SignIn({ isSignInOpen, setSignInOpen, setSignUpOpen, mod
     const { email, password } = formData;
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
+      const response = await fetch(`https://configurateur-production.up.railway.app/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
